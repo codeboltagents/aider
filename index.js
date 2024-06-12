ea8a83137c631c9c5635bfd2ba2db1277d4cc645
Utkarsh Shukla <utk.shukla@gmail.com>
2024-06-12 11:32:44 +0530

    Added Aider Js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const codebolt = require("@codebolt/codeboltjs").default;
-const path = require("path")
-const {CodeExecute} = require("./agents/CodeExecute")
+const codebolt = require('@codebolt/codeboltjs').default;
+
 async function execute() {
-
     await codebolt.waitForConnection();
-    var message = await codebolt.chat.waitforReply("Hi, I am AI Software Developer Agent,how can I assist you.");
-    codebolt.chat.processStarted();
- 
-    const GetExecute = await CodeExecute(message.message);
-    codebolt.chat.stopProcess();
 
-    var message = await codebolt.chat.waitforReply(GetExecute);
-    await execute();
+    // Enter your Code Here. For documentation see: https://docs.codebolt.ai
+    // Example:
+
 }
 
-(async () => {
-	await execute();
-})();
+(async () => { await execute(); })();
